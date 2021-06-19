fe86f89f2ec5ee7e14824c2800248fe68ff9028d
angelo <aurtula1@gmail.com>
2021-06-19 03:36:00 -0400

    current controls don't allow for multiple keydown

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,23 @@
 let player1Angle = 0
+
 document.addEventListener('keydown', function(e) {
-    let player1 = document.getElementById("player1");
+    ;
     let player1Weapon = document.getElementById("player1Weapon");
     
     if (e.key === " "){
         player1Angle += 10
-	player1.style.transform = `rotate(${player1Angle}deg)`;
-    player1Weapon.style.transform = `rotate(-${player1Angle}deg)`
-}
-});
+	    player1.style.transform = `rotate(${player1Angle}deg)`;
+        player1Weapon.style.transform = `rotate(-${player1Angle}deg)`
+    }else if(e.key === "ArrowRight"){
+        movePlayer1Right()
+        }
+    }
+);
+
+    function movePlayer1Right() {
+        let leftNumbers = player1.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+        
+        if (left < 1000) {
+        player1.style.left = `${left + 10}px`;
+        }}
