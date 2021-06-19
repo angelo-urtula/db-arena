c99fcb4e2b8366fa5b01a17048f1f9081517bf39
angelo <aurtula1@gmail.com>
2021-06-19 04:05:46 -0400

    refactoring movement

--- frontend/index.js
+++ frontend/index.js
@@ -31,21 +31,12 @@ let fight = {
 document.addEventListener('keydown', function(e) {
     
     let player1Weapon = document.getElementById("player1Weapon");
-    
     if (e.key === " "){
         player1Angle += 10
 	    player1.style.transform = `rotate(${player1Angle}deg)`;
-        player1Weapon.style.transform = `rotate(-${player1Angle}deg)`
-    }else if(e.key === "ArrowRight"){
+        player1Weapon.style.transform = `rotate(-${player1Angle}deg)`}
+    if(e.key === "ArrowRight"){
         movePlayer1Right()
         }
     }
 );
-
-    function movePlayer1Right() {
-        let leftNumbers = player1.style.left.replace("px", "");
-        let left = parseInt(leftNumbers, 10);
-        
-        if (left < 1000) {
-        player1.style.left = `${left + 10}px`;
-        }}
