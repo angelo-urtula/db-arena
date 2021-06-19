67d96234ac1dc3ab88e545ebf0167f3c8bc401c9
angelo <aurtula1@gmail.com>
2021-06-19 02:33:44 -0400

    adjusted animation speed

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,7 @@ document.addEventListener('keydown', function(e) {
     let player1Weapon = document.getElementById("player1Weapon");
     
     if (e.key === " "){
-        player1Angle += 120
+        player1Angle += 10
 	player1.style.transform = `rotate(${player1Angle}deg)`;
     player1Weapon.style.transform = `rotate(-${player1Angle}deg)`
 }
