399519c2f3a3c4870bd3706fad238872d51d4755
angelo <aurtula1@gmail.com>
2021-06-19 21:26:14 -0400

    added nested routes to represent relationships

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :souls
   resources :kills
+  resources :gladiators do
+    resources :souls, only: [:create, :destroy]
+    resources :kills, only: [:create, :update, :index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
