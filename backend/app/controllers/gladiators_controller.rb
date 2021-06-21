030fae8d67d14b76e02dfc280d3dcc6076244122
angelo <aurtula1@gmail.com>
2021-06-21 05:59:06 -0400

    updating gladiator controller

--- backend/app/controllers/gladiators_controller.rb
+++ backend/app/controllers/gladiators_controller.rb
@@ -4,13 +4,18 @@ class GladiatorsController < ApplicationController
         render json: gladiators.to_json(include: [:kills, :souls])
     end
 
-    def create
-        @gladiator = Gladiator.create(
-            name: params[:name],
-            motto: params[:motto],
-            reason: params[:reason],
-            honor: params[:honor]
-        )
+    def show
         render json: @gladiator
     end
+    
+     
+    def create
+        @gladiator = Gladiator.new(gladiator_params)
+    
+        if @gladiator.save
+          render json: @gladiator, status: :created, location: @gladiator
+        else
+          render json: @gladiator.errors, status: :unprocessable_entity
+        end
+      end
 end
