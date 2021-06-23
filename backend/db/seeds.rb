52191f194a69d6c68958245cf99838b5fe957ea7
angelo <aurtula1@gmail.com>
2021-06-23 01:31:28 -0400

    seeding database

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Gladiator.destroy_all
+Kill.destroy_all
+
+Gladiator.create(:name => "Maximus", :honor => "Loyalty", :motto => "ARE YOU NOT ENTERTAINED?", :reason => "Revenge")
+Gladiator.create(:name => "Mad Max", :honor => "There is no honor.", :motto => "My world is fire and blood.", :reason => "Survival")
+
+Kill.create(:gladiator_id => 1, :message => "Brothers, what we do in life echoes in eternity.")
+Kill.create(:gladiator_id => 1, :message => "Strength and honor!")
+Kill.create(:gladiator_id => 1, :message => "Death smiles at us all. All a man can do is smile back.")
+Kill.create(:gladiator_id => 2, :message => "Hope is a mistake.")
+Kill.create(:gladiator_id => 2, :message => "Once I was a cop.")
